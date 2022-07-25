562d90b519b707341a574716631d72d9babec397
Danila Polevshchikov <dpp1997@rambler.ru>
2022-07-25 20:16:52 +0700

    Delete spaceship image copy

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,7 +13,7 @@ class AlienInvasion():
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Alien Invasion by DanPolev")
         self.ship = Ship(self)
 
     def _check_events(self):
