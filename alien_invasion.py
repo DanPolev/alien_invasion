30b120ddb7c678d02ea49c6eb7841cb1e51a6724
Danila Polevshchikov <dpp1997@rambler.ru>
2022-07-25 17:26:01 +0700

    add Settings class to AlienInvasion class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,31 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion():
     """Main class for game behaviour and resources management"""
 
     def __init__(self):
+        """Initialize game and create game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1024, 720))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Run main loop"""
         while True:
-            """Tracking keyboard and mouse events"""
+            # Tracking keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            """Display last drawn screen"""
+            # Redraw the screen with given color
+            self.screen.fill(self.settings.bg_color)
+
+            # Display last drawn screen
             pygame.display.flip()
 
 if __name__ == "__main__":
