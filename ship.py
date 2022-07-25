f83193ec7446f9fc25d90f98d29ac9e02b1990e6
Danila Polevshchikov <dpp1997@rambler.ru>
2022-07-25 20:07:22 +0700

    add region boundaries: ship stops at the left & right boundaries

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship():
         """Initialize the ship, set its initial position"""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load spaceship image, get rectangle
         self.image = pygame.image.load("images/spaceship.bmp")
@@ -15,6 +16,10 @@ class Ship():
         # Set initial ship position
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # x-coordinate
+        self.x = float(self.rect.x)
+
+        # Moving flags
         self.moving_right = False
         self.moving_left = False
 
@@ -24,7 +29,8 @@ class Ship():
 
     def update(self):
         """Check moving flag, move the ship until flag is False"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
