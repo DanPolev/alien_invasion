c12815df493cc27a7f8f201284d1d0316b96b7ce
Danila Polevshchikov <dpp1997@rambler.ru>
2022-07-25 17:25:26 +0700

    add Settings class

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Class containing game configuration"""
+
+    def __init__(self, screen_width = 1024, screen_height = 720):
+        """Initialize game settings"""
+        # Screen settings
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.bg_color = (0, 0, 0)
