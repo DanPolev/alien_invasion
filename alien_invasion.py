281afb0a5d9ccbca53a8f0c63f7d8793918ffd0f
Danila Polevshchikov <dpp1997@rambler.ru>
2022-07-27 15:52:59 +0700

    add quiting the game with pressing ESCAPE key

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,7 +39,8 @@ class AlienInvasion():
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
 
     def _update_screen(self):
         """Update screen state"""
