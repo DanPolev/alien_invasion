b1bc34ab843a5e7cfa3c70331564aa9df6478dd8
Danila Polevshchikov <dpp1997@rambler.ru>
2022-07-27 16:02:20 +0700

    game launches in fullscreen mode

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 class Settings():
     """Class containing game configuration"""
 
-    def __init__(self, screen_width = 1024, screen_height = 720):
+    def __init__(self):
         """Initialize game settings"""
         # Screen settings
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+        self.screen_width = 1024
+        self.screen_height = 720
         self.bg_color = (0, 0, 0)
-        self.ship_speed = 0.5
+        self.ship_speed = 1
